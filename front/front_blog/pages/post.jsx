1863031fc05d60d3e34a83a71a21dc3a1356bf77
saamo24 <sabrahamyan.004@gmail.com>
2024-08-15 22:58:05 +0400

    added delete button

--- front/front_blog/pages/post.jsx
+++ front/front_blog/pages/post.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import { getCookie, deleteCookie, setCookie } from 'cookies-next';
 import Link from 'next/link';
 import CreatePostPopup from '../components/CreatePostPopup';
-import EditPostPopup from '../components/EditPostPopup'; // Import your EditPostPopup component
+import EditPostPopup from '../components/EditPostPopup';
 
 const BaseURL = 'http://localhost:8000';
 
@@ -14,9 +14,11 @@ const PostPage = ({ initialPosts, initialUsername }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [posts, setPosts] = useState(initialPosts);
   const [username, setUsername] = useState(initialUsername);
-  const [isPopupOpen, setIsPopupOpen] = useState(false); // State to control popup visibility
-  const [editPost, setEditPost] = useState(null); // State to manage which post to edit
-  const [isEditPopupOpen, setIsEditPopupOpen] = useState(false); // State to control edit popup visibility
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [editPost, setEditPost] = useState(null);
+  const [isEditPopupOpen, setIsEditPopupOpen] = useState(false);
+  const [deletePost, setDeletePost] = useState(null);
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -38,7 +40,7 @@ const PostPage = ({ initialPosts, initialUsername }) => {
         { refresh_token: localStorage.getItem('refreshToken') },
         {
           headers: {
-            'Authorization': `Bearer ${accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
           },
           withCredentials: true,
@@ -66,7 +68,7 @@ const PostPage = ({ initialPosts, initialUsername }) => {
   };
 
   const handlePostUpdated = (updatedPost) => {
-    setPosts(posts.map(post => post.pid === updatedPost.pid ? updatedPost : post));
+    setPosts(posts.map(post => (post.pid === updatedPost.pid ? updatedPost : post)));
     setIsEditPopupOpen(false);
     router.reload();
   };
@@ -76,7 +78,28 @@ const PostPage = ({ initialPosts, initialUsername }) => {
     setIsEditPopupOpen(true);
   };
 
-  const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date)); // Sort posts by date in descending order
+  const handleDeleteClick = (post) => {
+    setDeletePost(post);
+    setIsDeletePopupOpen(true);
+  };
+
+  const handleDeletePost = async () => {
+    try {
+      await axios.delete(`${BaseURL}/api/post/${deletePost.pid}/`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      setPosts(posts.filter(post => post.pid !== deletePost.pid));
+      setIsDeletePopupOpen(false);
+      router.reload();
+    } catch (error) {
+      console.error('Error deleting post:', error);
+    }
+  };
+
+  const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <Layout pageTitle="Posts" handleLogout={isAuthenticated ? handleLogout : null}>
@@ -98,6 +121,36 @@ const PostPage = ({ initialPosts, initialUsername }) => {
           onPostUpdated={handlePostUpdated}
         />
       )}
+  {isDeletePopupOpen && (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 1000
+    }}>
+      <div style={{
+        backgroundColor: 'black',
+        padding: '1.5rem',
+        borderRadius: '0.5rem',
+        border: '1px solid #0ced6a',
+        boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.1)',
+        maxWidth: '24rem',
+        width: '100%',
+        textAlign: 'center'
+      }}>
+        <p>Are you sure you want to delete this post?</p>
+        <button onClick={handleDeletePost} style={{ marginRight: '10px' }}>Yes</button>
+        <button onClick={() => setIsDeletePopupOpen(false)}>No</button>
+      </div>
+    </div>
+  )}
+
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
         {isAuthenticated ? (
           <>
@@ -109,7 +162,7 @@ const PostPage = ({ initialPosts, initialUsername }) => {
         <div>
           {posts.length > 0 ? (
             posts.map((post) => (
-              <div key={post.pid} style={{ marginBottom: '20px', border: '1px solid #0ced6a', padding: '10px', borderRadius: '15px' }}>
+              <div key={post.pid} style={{ marginBottom: '20px', border: '1px solid #0ced6a', padding: '10px', borderRadius: '15px', position: 'relative' }}>
                 <p>{post.content}</p>
                 <p>Date: {post.date}</p>
                 <p>
@@ -120,9 +173,17 @@ const PostPage = ({ initialPosts, initialUsername }) => {
                   </Link>
                 </p>
                 {isAuthenticated && post.poster_username === username && (
-                  <button onClick={() => handleEditClick(post)} style={{ marginTop: '10px' }}>
-                    Edit
-                  </button>
+                  <>
+                    <button onClick={() => handleEditClick(post)} style={{ marginTop: '10px' }}>
+                      Edit
+                    </button>
+                    <span
+                      onClick={() => handleDeleteClick(post)}
+                      style={{ position: 'absolute', bottom: '10px', right: '10px', fontSize: '24px', cursor: 'pointer'}}
+                    >
+                      🚮
+                    </span>
+                  </>
                 )}
               </div>
             ))
@@ -146,9 +207,9 @@ export async function getServerSideProps(context) {
     try {
       const response = await axios.get(`${BaseURL}/api/post/`, {
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
-        }
+        },
       });
       return response.data;
     } catch (error) {
